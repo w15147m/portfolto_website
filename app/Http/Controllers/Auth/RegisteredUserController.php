3023b57a5ecd331f869fbb672642366de75a2501
waseem abbas <waseemofficee@gmail.com>
2025-03-04 04:12:26 +0500

    Sanctum

--- app/Http/Controllers/Auth/RegisteredUserController.php
+++ app/Http/Controllers/Auth/RegisteredUserController.php
@@ -22,29 +22,33 @@ class RegisteredUserController extends Controller
         return view('auth.register');
     }
 
-    /**
-     * Handle an incoming registration request.
-     *
-     * @throws \Illuminate\Validation\ValidationException
-     */
-    public function store(Request $request): RedirectResponse
+    public function store(Request $request)
     {
         $request->validate([
             'name' => ['required', 'string', 'max:255'],
             'email' => ['required', 'string', 'lowercase', 'email', 'max:255', 'unique:'.User::class],
             'password' => ['required', 'confirmed', Rules\Password::defaults()],
         ]);
-
+    
         $user = User::create([
             'name' => $request->name,
             'email' => $request->email,
             'password' => Hash::make($request->password),
         ]);
-
+    
         event(new Registered($user));
-
+    
         Auth::login($user);
-
-        return redirect(route('dashboard', absolute: false));
+    
+        $token = $user->createToken('auth_token')->plainTextToken;
+    
+        session([
+            'token' => $token,
+            'authUser' => $user
+        ]);
+    
+        return redirect()->intended(route('dashboard', absolute: false));
     }
+    
+    
 }

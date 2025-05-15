412f3c22dc66bba88b0ff669165f6258895b7844
waseem abbas <waseemofficee@gmail.com>
2025-05-15 19:41:56 +0500

    Comment out the dashboard link in the sidebar for future reference and cleanup

--- resources/js/user/layout/sidebar/SidebarComponent.vue
+++ resources/js/user/layout/sidebar/SidebarComponent.vue
@@ -12,9 +12,9 @@
             <!-- Main Sections Links -->
             <div class="is-scrollbar-hidden flex grow flex-col space-y-4 overflow-y-auto pt-6">
                 <!-- Dashobards -->
-                <router-link to="/dashboard" class="flex size-11 items-center justify-center rounded-lg bg-primary/10 text-primary outline-none transition-colors duration-200 hover:bg-primary/20 focus:bg-primary/20 active:bg-primary/25 dark:bg-navy-600 dark:text-accent-light dark:hover:bg-navy-450 dark:focus:bg-navy-450 dark:active:bg-navy-450/90" x-tooltip.placement.right="'Dashboards'">
+                <!-- <router-link to="/dashboard" class="flex size-11 items-center justify-center rounded-lg bg-primary/10 text-primary outline-none transition-colors duration-200 hover:bg-primary/20 focus:bg-primary/20 active:bg-primary/25 dark:bg-navy-600 dark:text-accent-light dark:hover:bg-navy-450 dark:focus:bg-navy-450 dark:active:bg-navy-450/90" x-tooltip.placement.right="'Dashboards'">
                     <i class="fa-solid fa-house"></i>
-                </router-link>
+                </router-link> -->
 
                 <!-- Apps -->
                 <router-link to="/dashboard/portfolio" class="flex size-11 items-center justify-center rounded-lg outline-none transition-colors duration-200 hover:bg-primary/20 focus:bg-primary/20 active:bg-primary/25 dark:hover:bg-navy-300/20 dark:focus:bg-navy-300/20 dark:active:bg-navy-300/25" x-tooltip.placement.right="'Portfolio'">
